6f92e7bdf05a681222d966b16b70ea540a433d02
SHAGHAYEGH KHALIGHIYAN <khaliqianshaqayeq@gmail.com>
2025-11-02 15:22:43 -0500

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-import json
-def format_example(rec):
-    instr = rec.get("instruction","").strip()
-    inp = rec.get("input","").strip()
-    out = rec.get("output","").strip()
-    if inp:
-        return f"### Instruction:\n{instr}\n\n### Input:\n{inp}\n\n### Response:\n{out}"
-    else:
-        return f"### Instruction:\n{instr}\n\n### Response:\n{out}"
